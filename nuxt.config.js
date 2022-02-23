a9b6a3c1f729d7e02003fd8ebe014cc203d3b1c4
Robin Malfait <malfait.robin@gmail.com>
2022-02-23 15:29:06 +0100

    setup tailwindcss and autoprefixer

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,5 +30,12 @@ export default {
   modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  },
 };
