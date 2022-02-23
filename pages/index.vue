1ee62489c02ad2e4f565a354b0e54f4664a0a0bc
Robin Malfait <malfait.robin@gmail.com>
2022-02-23 15:37:53 +0100

    add example prose

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,19 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <main class="prose bg-gray-50 mx-auto p-12 m-12 rounded-xl">
+    <h1>H1</h1>
+    <h2>H2</h2>
+    <h3>H3</h3>
+    <blockquote>Some quote</blockquote>
+    <ul>
+      <li>List item 1</li>
+      <li>List item 2</li>
+      <li>List item 3</li>
+    </ul>
+  </main>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
